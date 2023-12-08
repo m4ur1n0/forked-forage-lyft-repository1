ae4a49c2aab2ea8e1dceeff8e906f5f881840a8d
theo <125602028+m4ur1n0@users.noreply.github.com>
2023-12-08 13:37:31 -0600

    Add files via upload

---
+++ test/updated_test.py
@@ -0,0 +1,13 @@
+import sys
+from datetime import datetime
+
+sys.path.append("../forage-lyft-starter-repo")
+
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
+
+from engine.battery.nubbin_battery import NubbinBattery
+from engine.battery.spindler_battery import SpindlerBattery
+
+
